5b327a8573f9300e7ad5627f1ca92d6b66a179ff
Jim Finn <jim.finnjim2001@gmail.com>
2023-05-09 10:01:37 +0100

    Added Lesson 01 Files

---
+++ TY Intro to Python Course/Lesson 01/Solution Files/Exercise 01.py
@@ -0,0 +1,9 @@
+# Task 01
+print("Hello World")
+
+# Task 02
+print("Question. What do you call it when you have your mom's mom on social media?")  
+print("Answer. Instagran")
+
+# Task 03
+print("\U0001f600")
