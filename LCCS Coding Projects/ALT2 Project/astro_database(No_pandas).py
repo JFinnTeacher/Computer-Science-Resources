b8ec0c0df8b0f31a236356ed83ef1f3ad3b9cb37
J Finn <jim.finnjim2001@gmail.com>
2025-12-14 13:08:45 +0000

    Added ALT2 Project Code

---
+++ LCCS Coding Projects/ALT2 Project/astro_database(No_pandas).py
@@ -0,0 +1,21 @@
+'''
+Title: ALT2 Project using Database of Astronaut Information without using Pandas
+Author: J Finn
+Date: Dec 2025
+'''
+
+# Import necessary libraries
+
+# Load the CSV File 
+
+# Process the data to extract information of time spent in space by country
+
+    # Show Visualization of time spent in space by country
+
+# Process the data to extract the information by gender
+
+    # Show Visualization of time spent in space by gender
+
+# Process the data to extract the information of country by gender
+
+    # Show Visualization of country by gender  
