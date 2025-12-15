03112f2ce826fb5d47942bcf7da0b424c0522be8
Jim Finn <jim.finnjim2001@gmail.com>
2025-12-15 13:38:21 +0000

    updated project file

--- LCCS Coding Projects/ALT2 Project/astro_database(No_pandas).py
+++ LCCS Coding Projects/ALT2 Project/astro_database(No_pandas).py
@@ -5,17 +5,129 @@ Date: Dec 2025
 '''
 
 # Import necessary libraries
+import csv
+import matplotlib.pyplot as plt
+
+# Initilise the required lists for processing
+names = []
+countries = []
+genders = []
+flights = []
+total_flights = []
+flight_times = []
 
 # Load the CSV File 
+with open("astrodbase.csv", "r") as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)  # Skip header
+    
+    for row in csv_reader: # append row[0] to names etc
+        names.append(row[0])
+        countries.append(row[1])
+        genders.append(row[2])
+        flights.append(row[3])
+        total_flights.append(row[4])
+        
+        # Flight times in the form of day,hours,mins This needs to be converted to
+        # a single value of hours
+        time_parts = row[5].split(":")
+        days = int(time_parts[0])
+        hours = int(time_parts[1])
+        mins = int(time_parts[2])
+        total_hours = ((days*24) + hours)
+        
+        #add the calculated value to flight times variable
+        flight_times.append(total_hours)
 
 # Process the data to extract information of time spent in space by country
+# Start by creating 2 lists - 1 to record the countries and 1 to store the time in space.
+# If the code finds a country that is not on the list then add that country and add the hours
 
-    # Show Visualization of time spent in space by country
+unique_countries = []
+time_in_space =[]
+location = 0
+
+for i in range (len(countries)):
+    country = countries[i]
+    hours = flight_times[i]
+    
+    # Is country already on the list
+    if country in unique_countries:
+        location = unique_countries.index(country)
+        time_in_space[location] = time_in_space[location] + hours
+    else:
+        unique_countries.append(country)
+        time_in_space.append(hours)
+
+#sort the lists in decending order
+
+for i in range(len(time_in_space)):
+    #find the position of the maximum time in space
+    max_position = i
+    for j in range(i +1, len(time_in_space)):
+        #Check if the current value is larger than the maximum value
+        if time_in_space[j] > time_in_space[max_position]:
+            max_position = j #Sets the position to the maximum value
+            
+    # Move the time in space value to the current max value
+    temp = time_in_space[i]
+    time_in_space[i] = time_in_space[max_position]
+    time_in_space[max_position] = temp
+        
+    #Swap the countries using the same values
+    temp = unique_countries[i]
+    unique_countries[i] = unique_countries[max_position]
+    unique_countries[max_position] = temp
+
+
+# Show Visualization of time spent in space by country
+plt.figure(figsize=(12, 6))
+bars = plt.bar(unique_countries, time_in_space)
+
+# Add value labels on top of each bar
+for i in range(len(unique_countries)):
+    plt.text(i, time_in_space[i], str(time_in_space[i]), 
+             ha='center', va='bottom', rotation=90)
+
+plt.xlabel("Country")
+plt.ylabel("Hours in Space")
+plt.title("Total Time in Space by Country")
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
 
 # Process the data to extract the information by gender
+unique_gender = []
+time_in_space =[]
+location = 0
 
-    # Show Visualization of time spent in space by gender
+for i in range (len(genders)):
+    gender = genders[i]
+    hours = flight_times[i]
+    
+    # Is country already on the list
+    if gender in unique_gender:
+        location = unique_gender.index(gender)
+        time_in_space[location] = time_in_space[location] + hours
+    else:
+        unique_gender.append(gender)
+        time_in_space.append(hours)
+# Show Visualization of time spent in space by gender
 
+plt.figure(figsize=(12, 6))
+bars = plt.bar(unique_gender, time_in_space)
+
+# Add value labels on top of each bar
+for i in range(len(unique_gender)):
+    plt.text(i, time_in_space[i], str(time_in_space[i]), 
+             ha='center', va='bottom')
+
+plt.xlabel("Gender")
+plt.ylabel("Hours in Space")
+plt.title("Total Time in Space by Gender")
+plt.tight_layout()
+plt.show()
+    
 # Process the data to extract the information of country by gender
 
-    # Show Visualization of country by gender  
+# Show Visualization of country by gender  
