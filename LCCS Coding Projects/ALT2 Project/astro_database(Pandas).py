7535e546e4852f260d3df904eaae9f87761055a3
Jim Finn <jim.finnjim2001@gmail.com>
2025-12-16 10:31:42 +0000

    Add total hours calculation and country time visualization
    Introduced a function to convert flight time to total hours and added a pie chart visualization of total hours spent in space by country using matplotlib.

--- LCCS Coding Projects/ALT2 Project/astro_database(Pandas).py
+++ LCCS Coding Projects/ALT2 Project/astro_database(Pandas).py
@@ -6,13 +6,23 @@ Date: Dec 2025
 
 # Import necessary libraries
 import pandas as pd
+import matplotlib.pyplot as plt
 # Load the CSV File 
 df = pd.read_csv('astrodbase.csv')
-print(df['Total Flight Time (ddd:hh:mm)'].head())
+
 # Process the data to extract information of time spent in space by country
+def convert_to_hours(time_space):
+    parts = time_space.split(":")
+    total_hours = (int(parts[0])*24) + int(parts[1])
+    return total_hours
+
+df['Total Hours'] = df['Total Flight Time (ddd:hh:mm)'].apply(convert_to_hours)
 
 # Show Visualization of time spent in space by country
+print(df.groupby('Country')['Total Hours'].sum().sort_values(ascending=False))
 
+plt.pie(time_in_space, labels=unique_countries, autopct='%1.0f%%', 
+        textprops={'fontsize': 8})
 # Process the data to extract the information by gender
 
 # Show Visualization of time spent in space by gender
